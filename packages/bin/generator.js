ece87aac660b6a60f66e92ba089c75d91750a4b5
AshtonYoon <y109384@naver.com>
2025-05-02 14:19:52 +0900

    주석 제거, bin 파일 위치 수정

--- packages/bin/generator.js
+++ packages/bin/generator.js
@@ -1,5 +1,3 @@
-// generator.js
-// #! /usr/bin/env node
 const { execSync } = require("child_process");
 const path = require("path");
 const fs = require("fs");
@@ -22,7 +20,6 @@ if (!isCurrentPathProject) {
     fs.mkdirSync(projectPath);
   } catch (err) {
     if (err.code === "EXIST") {
-      // 이미 해당 경로 존재
       console.log(
         `[ERROR]: The file ${projectName} already exist in the current directory.`
       );
@@ -35,28 +32,22 @@ if (!isCurrentPathProject) {
 
 async function generator() {
   try {
-    // git clone
     console.log("[INFO]: Downloading ssu-catch-lyrics...");
     execSync(`git clone ${GIT_REPO} ${tempPath}`);
 
-    // 임시 폴더에서 react-boilerplate만 복사
     console.log("[INFO]: Copying files...");
     copySync(`${tempPath}/react-boilerplate`, projectPath);
 
-    // 임시 폴더 삭제
     removeSync(tempPath);
 
-    // 현재 경로 이동
     console.log("[INFO]: Moving into directory...");
     if (!isCurrentPathProject) {
       process.chdir(projectPath);
     }
 
-    // 의존성 설치
     console.log("[INFO]: install dependencies...");
     execSync("npm install");
 
-    // SUCCESS !
     console.log("[SUCCESS]: Success to ssu-catch-lyrics. Available now !");
   } catch (error) {
     console.log(error);
